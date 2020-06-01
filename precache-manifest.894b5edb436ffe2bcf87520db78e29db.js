7ad57293f0c9e8934090f49e5e1dcd429e960e09
Krishna Thorat <krishnathorat007@gmail.com>
2020-06-01 23:37:14 +0530

    Updates

---
+++ precache-manifest.894b5edb436ffe2bcf87520db78e29db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff1a39c75774887c8e0d63bab118c4e",
+    "url": "/saurabh-portfolio/index.html"
+  },
+  {
+    "revision": "c5eaf0d904afb6b9262f",
+    "url": "/saurabh-portfolio/static/css/main.9b064583.chunk.css"
+  },
+  {
+    "revision": "7e2cfa70736909a6b289",
+    "url": "/saurabh-portfolio/static/js/2.db622a8b.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/saurabh-portfolio/static/js/2.db622a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5eaf0d904afb6b9262f",
+    "url": "/saurabh-portfolio/static/js/main.56259431.chunk.js"
+  },
+  {
+    "revision": "66db0620ba756846572b",
+    "url": "/saurabh-portfolio/static/js/runtime-main.8b919f1b.js"
+  },
+  {
+    "revision": "85560531a008e5a75d614c39638b7c55",
+    "url": "/saurabh-portfolio/static/media/saurabh.85560531.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/saurabh-tanpure/precache-manifest.0642db153eae98d37e28c129cfc39435.js"
+  "/saurabh-portfolio/precache-manifest.894b5edb436ffe2bcf87520db78e29db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saurabh-tanpure/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saurabh-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
