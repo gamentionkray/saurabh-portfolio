7eb641240e39f63739917eec53a7fc1e5af1d418
Krishna Thorat <krishnathorat007@gmail.com>
2020-06-01 23:19:14 +0530

    Updates

---
+++ precache-manifest.ce9f83cce221d292a4c112173879182f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d814a04a647d5e05a12004bab367f183",
+    "url": "/saurabh-portfolio/index.html"
+  },
+  {
+    "revision": "ce362c996eba58597087",
+    "url": "/saurabh-portfolio/static/css/main.9b064583.chunk.css"
+  },
+  {
+    "revision": "146460dcaaba2bbf39e8",
+    "url": "/saurabh-portfolio/static/js/2.0e361950.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/saurabh-portfolio/static/js/2.0e361950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce362c996eba58597087",
+    "url": "/saurabh-portfolio/static/js/main.f9d3403d.chunk.js"
+  },
+  {
+    "revision": "66db0620ba756846572b",
+    "url": "/saurabh-portfolio/static/js/runtime-main.8b919f1b.js"
+  },
+  {
+    "revision": "85560531a008e5a75d614c39638b7c55",
+    "url": "/saurabh-portfolio/static/media/saurabh.85560531.jpeg"
+  }
+]);
