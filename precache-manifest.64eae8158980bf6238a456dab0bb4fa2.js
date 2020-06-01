b96d3a31df283c28212027d014b129d6c48aa9c4
Krishna Thorat <krishnathorat007@gmail.com>
2020-06-01 23:21:59 +0530

    Updates

--- precache-manifest.64eae8158980bf6238a456dab0bb4fa2.js
+++ precache-manifest.64eae8158980bf6238a456dab0bb4fa2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d814a04a647d5e05a12004bab367f183",
+    "revision": "6cf0c9f0c65052862babfe2e97a4e33d",
     "url": "/saurabh-portfolio/index.html"
   },
   {
-    "revision": "ce362c996eba58597087",
+    "revision": "c5eaf0d904afb6b9262f",
     "url": "/saurabh-portfolio/static/css/main.9b064583.chunk.css"
   },
   {
-    "revision": "146460dcaaba2bbf39e8",
-    "url": "/saurabh-portfolio/static/js/2.0e361950.chunk.js"
+    "revision": "7e2cfa70736909a6b289",
+    "url": "/saurabh-portfolio/static/js/2.db622a8b.chunk.js"
   },
   {
     "revision": "ddb934a41eb6bf13706c65fe3758fb10",
-    "url": "/saurabh-portfolio/static/js/2.0e361950.chunk.js.LICENSE.txt"
+    "url": "/saurabh-portfolio/static/js/2.db622a8b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ce362c996eba58597087",
-    "url": "/saurabh-portfolio/static/js/main.f9d3403d.chunk.js"
+    "revision": "c5eaf0d904afb6b9262f",
+    "url": "/saurabh-portfolio/static/js/main.56259431.chunk.js"
   },
   {
     "revision": "66db0620ba756846572b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/saurabh-portfolio/precache-manifest.ce9f83cce221d292a4c112173879182f.js"
+  "/saurabh-portfolio/precache-manifest.64eae8158980bf6238a456dab0bb4fa2.js"
 );
 
 self.addEventListener('message', (event) => {
